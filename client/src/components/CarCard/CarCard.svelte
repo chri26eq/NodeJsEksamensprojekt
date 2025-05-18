31cf3d62a26cf0936ba4f00919517fe1a2a56420
Christoffer <chri26eq@stud.kea.dk>
2025-05-18 13:02:58 +0200

    car coloring

--- client/src/components/CarCard/CarCard.svelte
+++ client/src/components/CarCard/CarCard.svelte
@@ -7,11 +7,11 @@
   const { car, preview = false } = $props();
 
   const cardColors = {
-    pp00_39: { topBar: "bg-neutral-300", sideBar: "", mainBody: "", bottom: "", ppBox: "" },
-    pp40_59: { topBar: "bg-lime-300", sideBar: "", mainBody: "", bottom: "", ppBox: "" },
-    pp60_79: { topBar: "bg-red-600", sideBar: "", mainBody: "", bottom: "", ppBox: "" },
-    pp80_89: { topBar: "bg-purple-500", sideBar: "", mainBody: "", bottom: "", ppBox: "" },
-    pp90_100: { topBar: "bg-amber-300", sideBar: "", mainBody: "", bottom: "", ppBox: "" },
+    pp00_39: { topBar: " bg-neutral-300 ", sideBar: " bg-neutral-50 border-l-1 ", mainBody: " bg-neutral-50 ", bottom: " bg-neutral-200 "},
+    pp40_59: { topBar: " bg-lime-300 ", sideBar: " bg-neutral-50 border-l-1 ", mainBody: " bg-neutral-50 ", bottom: " bg-neutral-200 "},
+    pp60_79: { topBar: " bg-red-600 ", sideBar: " bg-neutral-50 border-l-1 ", mainBody: " bg-neutral-800 text-white ", bottom: " bg-neutral-600 text-white "},
+    pp80_89: { topBar: " bg-violet-700 text-white ", sideBar: " bg-teal-200 ", mainBody: " bg-neutral-800 text-white ", bottom: " bg-teal-400 "},
+    pp90_100: { topBar: " bg-amber-300 ", sideBar: " bg-cyan-500 ", mainBody: " bg-cyan-300 ", bottom: " bg-amber-300 "},
   };
 
   let cardColor = $derived(() => {
@@ -24,30 +24,30 @@
 
 </script>
 
-<div class="flex flex-col h-70 w-45 shadow-xl/30 rounded-md overflow-hidden">
-  <div class={"flex flex-1 items-center " + cardColor().topBar}>
+<div class="flex flex-col h-70 w-45 shadow-xl/30 rounded-md overflow-hidden ">
+  <div class={"flex flex-1 items-center" + cardColor().topBar}>
     <p class="text-[0.9em] font-bold mx-1">{car.brand_name}</p>
   </div>
-  <div class="flex flex-6 bg-amber-700">
-    <div class="bg-cyan-300 flex flex-col flex-7">
-      <div class="flex flex-col flex-1 text-sm text-gray-700">
+  <div class="flex flex-6">
+    <div class={"flex flex-col flex-7" + cardColor().mainBody}>
+      <div class="flex flex-col flex-1 text-sm">
         <p class="font-semibold px-1 py-0.5 break-words">{car.model_name}</p>
       </div>
-      <div class="flex flex-row items-end justify-between h-full">
+      <div class="flex flex-row items-end justify-between h-full ">
         <p
-          class="text-[0.9em] text-gray-700 font-semibold px-1 py-0.5 break-words max-w-[70%]"
+          class="text-[0.9em] font-semibold px-1 py-0.5 break-words max-w-[70%]"
         >
           {car.country_name}
         </p>
 
         <p
-          class="text-[0.8em] bold font-semibold whitespace-nowrap bg-amber-200 px-1 py-0.5 rounded m-1"
+          class="text-[0.8em] bold font-semibold whitespace-nowrap bg-amber-200 text-black px-1 py-0.5 rounded m-1"
         >
           {car.pp} PP
         </p>
       </div>
     </div>
-    <div class="flex flex-col bg-cyan-500 flex-1">
+    <div class={"flex flex-col flex-1" + cardColor().sideBar}>
       {#if !preview}
         <FavoriteButton {car} />
         <UpgradeButton {car} />
@@ -55,7 +55,7 @@
       {/if}
     </div>
   </div>
-  <div class="flex flex-4 bg-amber-500">
+  <div class={"flex flex-4" + cardColor().bottom}>
     <div class="flex-1 flex-col">
       <div class="border-t-1 py-0.5 px-1">
         <p class="text-[0.6em]">Topspeed:</p>
