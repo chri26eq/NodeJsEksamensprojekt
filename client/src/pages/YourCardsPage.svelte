ede298df7bdffeb270afbf978556df73fc9a8a24
Christoffer <chri26eq@stud.kea.dk>
2025-06-16 13:13:25 +0200

    button to redirect to store when you have no cards

--- client/src/pages/YourCardsPage.svelte
+++ client/src/pages/YourCardsPage.svelte
@@ -1,7 +1,20 @@
 <script>
+  import { navigate } from "svelte-routing";
   import CarList from "../components/CarList/CarList.svelte";
   import { userCars } from "../stores/userStore";
+  import { Button } from "flowbite-svelte";
+
+  function buttonHandler() {
+    navigate("/buycards", { replace: true });
+  }
 </script>
 
+{#if $userCars.length === 0}
+  <div class="flex flex-col items-center justify-center text-center">
+    <h1 class="text-4xl md:text-5xl font-bold mb-4">You have no CarCards</h1>
+    <Button size="xl" color="green" onclick={buttonHandler}>Go to store</Button>
+  </div>
+{:else}
+  <CarList cars={$userCars} />
+{/if}
 
-<CarList cars={$userCars} />
