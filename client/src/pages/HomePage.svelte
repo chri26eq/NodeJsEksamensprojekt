c06816ff72f6f0ff3292c2a5003495c016d87f96
Christoffer <chri26eq@stud.kea.dk>
2025-06-16 13:07:09 +0200

    home-page

--- client/src/pages/HomePage.svelte
+++ client/src/pages/HomePage.svelte
@@ -1,37 +1,34 @@
 <div class="flex flex-row">
-  <div class="flex flex-1 flex-col">
-    <h1 style="font-size: 2.5rem; ">
+  <div class="flex flex-1 flex-col p-4">
+    <h1 class="text-4xl md:text-5xl font-bold mb-4">
       Do you dream of driving real cars against real players from all over the
       world?
     </h1>
-    <h2 style="font-size: 2rem; margin: 1rem 0;">
-      <strong>Your dream is about to come true!</strong>
+    <h2 class="text-3xl font-semibold my-4">
+      Your dream is about to come true!
     </h2>
 
-    <h3 style="font-size: 1.5rem; margin: 1.5rem 0 1rem 0;">Features:</h3>
-    <ul style="list-style: none; padding: 0; max-width: 600px;">
-      <li style="margin-bottom: 0.5rem;">
-        Hundreds of real cars from all over the world
-      </li>
-      <li style="margin-bottom: 0.5rem;">
+    <h3 class="text-2xl font-medium my-4">Features:</h3>
+    <ul class="list-none p-0 max-w-xl space-y-2">
+      <li>Hundreds of real cars from all over the world</li>
+      <li>
         Be strategic! Many different tracks with unique layouts and surfaces.
         Your choice of car makes all the difference.
       </li>
-      <li style="margin-bottom: 0.5rem;">
-        Race against real players in real-time and win CarCash.
-      </li>
-      <li style="margin-bottom: 0.5rem;">Use CarCash to buy rare cars!</li>
+      <li>Race against real players in real-time and win CarCash.</li>
+      <li>Use CarCash to buy rare cars!</li>
     </ul>
 
-    <p style="font-size: 1.5rem; margin-top: 2rem; ">
+    <p class="text-2xl mt-8 text-red-400">
       Welcome to <strong>CarCards</strong>!
     </p>
   </div>
-  <div class="flex flex-1">
+
+  <div class="flex flex-1 items-center justify-center p-4">
     <img
       src="/sport-car.png"
       alt="Sports car"
-      class="object-contain w-full px-[1em]"
+      class="object-contain w-full max-w-sm md:max-w-md lg:max-w-lg"
     />
   </div>
 </div>
